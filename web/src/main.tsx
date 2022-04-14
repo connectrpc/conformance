229ad4a03ff8389d9e843f902ff781c1c256092c
Tommy Ma <tma@buf.build>
2022-04-14 14:52:59 -0400

    empty web app for connect-web test

---
+++ web/src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  </React.StrictMode>
+)
