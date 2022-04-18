2e5f454ff1bb3289e23fef5e7a232808d9f45616
Doria Keung <doriable@users.noreply.github.com>
2022-04-18 11:35:13 -0400

    Add empty test cases for connect-web interop tests (#30)

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -12,13 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+const ignoreFiles = [
+  "dist/**",
+]
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: "esnext",
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
   },
-});
+  ignorePatterns: ignoreFiles,
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
