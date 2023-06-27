8a161ad95c82cf2d64412fbf45296354c0b4336f
Steve Ayers <sayers@buf.build>
2023-06-27 15:52:42 -0400

    gRPC web test fix

--- web/spec/grpc-web.spec.ts
+++ web/spec/grpc-web.spec.ts
@@ -54,7 +54,14 @@ function multiDone(done: DoneFn, count: number) {
 describe("grpc_web", function () {
   const host = __karma__.config.host;
   const port = __karma__.config.port;
-  const SERVER_HOST = `https://${host}:${port}`;
+  const insecure = __karma__.config.insecure;
+  let scheme = "";
+  if (insecure === "true" || insecure === true) {
+    scheme = "http://";
+  } else {
+    scheme = "https://";
+  }
+  const SERVER_HOST = `${scheme}${host}:${port}`;
   const client = new TestServiceClient(SERVER_HOST, null, null);
   it("empty_unary", function (done) {
     client.emptyCall(new Empty(), null, (err, response) => {
