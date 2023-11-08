84f6ac740179056c99356cd6c5b990829543c04c
Steve Ayers <smaye81@gmail.com>
2023-11-08 11:51:16 -0500

    V2: Implement rest of gRPC server endpoints (#638)
    This implements the rest of the endpoints for the gRPC server (client
    streaming, server streaming, and bidi)
    
    ---------
    
    Co-authored-by: Joshua Humphries <2035234+jhump@users.noreply.github.com>

--- internal/app/grpcserver/impl.go
+++ internal/app/grpcserver/impl.go
@@ -16,9 +16,14 @@ package grpcserver
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"time"
 
 	v1alpha1 "connectrpc.com/conformance/internal/gen/proto/go/connectrpc/conformance/v1alpha1"
 	"connectrpc.com/conformance/internal/grpcutil"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
@@ -44,11 +49,16 @@ func (c *conformanceServiceServer) Unary(
 		return nil, err
 	}
 
-	if err := grpcutil.AddHeaderMetadata(ctx, req.ResponseDefinition.ResponseHeaders); err != nil {
-		return nil, err
-	}
-	if err := grpcutil.AddTrailerMetadata(ctx, req.ResponseDefinition.ResponseTrailers); err != nil {
-		return nil, err
+	responseDefinition := req.ResponseDefinition
+	if responseDefinition != nil {
+		headerMD := grpcutil.ConvertProtoHeaderToMetadata(req.ResponseDefinition.ResponseHeaders)
+		if err := grpc.SetHeader(ctx, headerMD); err != nil {
+			return nil, err
+		}
+		trailerMD := grpcutil.ConvertProtoHeaderToMetadata(req.ResponseDefinition.ResponseTrailers)
+		if err := grpc.SetTrailer(ctx, trailerMD); err != nil {
+			return nil, err
+		}
 	}
 
 	md, _ := metadata.FromIncomingContext(ctx)
@@ -67,25 +77,201 @@ func (c *conformanceServiceServer) Unary(
 }
 
 func (c *conformanceServiceServer) ClientStream(
-	_ v1alpha1.ConformanceService_ClientStreamServer,
+	stream v1alpha1.ConformanceService_ClientStreamServer,
 ) error {
-	// TODO - Implement ClientStream
-	return status.Error(codes.Unimplemented, "method ClientStream not implemented")
+	var responseDefinition *v1alpha1.UnaryResponseDefinition
+	firstRecv := true
+	var reqs []*anypb.Any
+
+	for {
+		if err := stream.Context().Err(); err != nil {
+			return err
+		}
+		msg, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+		// If this is the first message received on the stream, save off the response definition we need to send
+		if firstRecv {
+			responseDefinition = msg.ResponseDefinition
+			firstRecv = false
+		}
+		// Record all the requests received
+		msgAsAny, err := asAny(msg)
+		if err != nil {
+			return err
+		}
+		reqs = append(reqs, msgAsAny)
+	}
+
+	// Set headers and trailers on stream
+	if responseDefinition != nil {
+		headerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseHeaders)
+		if err := stream.SetHeader(headerMD); err != nil {
+			return err
+		}
+
+		trailerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseTrailers)
+		stream.SetTrailer(trailerMD)
+	}
+
+	md, _ := metadata.FromIncomingContext(stream.Context())
+	payload, err := parseUnaryResponseDefinition(responseDefinition, md, reqs)
+	if err != nil {
+		return err
+	}
+
+	return stream.SendAndClose(&v1alpha1.ClientStreamResponse{
+		Payload: payload,
+	})
 }
 
 func (c *conformanceServiceServer) ServerStream(
-	_ *v1alpha1.ServerStreamRequest,
-	_ v1alpha1.ConformanceService_ServerStreamServer,
+	req *v1alpha1.ServerStreamRequest,
+	stream v1alpha1.ConformanceService_ServerStreamServer,
 ) error {
-	// TODO - Implement ServerStream
-	return status.Error(codes.Unimplemented, "method ServerStream not implemented")
+	responseDefinition := req.ResponseDefinition
+	if responseDefinition != nil {
+		headerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseHeaders)
+		if err := stream.SetHeader(headerMD); err != nil {
+			return err
+		}
+
+		trailerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseTrailers)
+		stream.SetTrailer(trailerMD)
+	}
+
+	// Convert the request to an Any so that it can be recorded in the payload
+	msgAsAny, err := asAny(req)
+	if err != nil {
+		return err
+	}
+
+	metadata, _ := metadata.FromIncomingContext(stream.Context())
+	requestInfo := createRequestInfo(metadata, []*anypb.Any{msgAsAny})
+	payload := &v1alpha1.ConformancePayload{
+		RequestInfo: requestInfo,
+	}
+
+	for _, data := range responseDefinition.ResponseData {
+		payload.Data = data
+
+		resp := &v1alpha1.ServerStreamResponse{
+			Payload: payload,
+		}
+
+		time.Sleep((time.Duration(responseDefinition.ResponseDelayMs) * time.Millisecond))
+
+		if err := stream.Send(resp); err != nil {
+			return status.Errorf(codes.Internal, "error sending on stream: %s", err.Error())
+		}
+		// Only echo back the request info in the first response
+		payload.RequestInfo = nil
+	}
+	if responseDefinition.Error != nil {
+		return grpcutil.ConvertProtoToGrpcError(responseDefinition.Error)
+	}
+	return nil
 }
 
 func (c *conformanceServiceServer) BidiStream(
-	_ v1alpha1.ConformanceService_BidiStreamServer,
+	stream v1alpha1.ConformanceService_BidiStreamServer,
 ) error {
-	// TODO - Implement BidiStream
-	return status.Error(codes.Unimplemented, "method BidiStream not implemented")
+	var responseDefinition *v1alpha1.StreamResponseDefinition
+	fullDuplex := false
+	firstRecv := true
+	respNum := 0
+	var reqs []*anypb.Any
+	for {
+		if err := stream.Context().Err(); err != nil {
+			return err
+		}
+		req, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				// Reads are done, break the receive loop and send any remaining responses
+				break
+			}
+			return fmt.Errorf("receive request: %w", err)
+		}
+
+		// Record all requests received
+		msgAsAny, err := asAny(req)
+		if err != nil {
+			return err
+		}
+		reqs = append(reqs, msgAsAny)
+
+		// If this is the first message in the stream, save off the total responses we need to send
+		// plus whether this should be full or half duplex
+		if firstRecv {
+			responseDefinition = req.ResponseDefinition
+			fullDuplex = req.FullDuplex
+			firstRecv = false
+
+			if responseDefinition != nil {
+				headerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseHeaders)
+				if err := stream.SetHeader(headerMD); err != nil {
+					return err
+				}
+
+				trailerMD := grpcutil.ConvertProtoHeaderToMetadata(responseDefinition.ResponseTrailers)
+				stream.SetTrailer(trailerMD)
+			}
+		}
+
+		// If fullDuplex, then send one of the desired responses each time we get a message on the stream
+		if fullDuplex {
+			if respNum >= len(responseDefinition.ResponseData) {
+				return status.Error(
+					codes.Aborted,
+					"received more requests than desired responses on a full duplex stream",
+				)
+			}
+			metadata, _ := metadata.FromIncomingContext(stream.Context())
+			requestInfo := createRequestInfo(metadata, reqs)
+			resp := &v1alpha1.BidiStreamResponse{
+				Payload: &v1alpha1.ConformancePayload{
+					RequestInfo: requestInfo,
+					Data:        responseDefinition.ResponseData[respNum],
+				},
+			}
+			time.Sleep((time.Duration(responseDefinition.ResponseDelayMs) * time.Millisecond))
+
+			if err := stream.Send(resp); err != nil {
+				return status.Errorf(codes.Internal, "error sending on stream: %s", err.Error())
+			}
+			respNum++
+			reqs = nil
+		}
+	}
+
+	// If we still have responses left to send, flush them now. This accommodates
+	// both scenarios of half duplex (we haven't sent any responses yet) or full duplex
+	// where the requested responses are greater than the total requests.
+	for ; respNum < len(responseDefinition.ResponseData); respNum++ {
+		metadata, _ := metadata.FromIncomingContext(stream.Context())
+		requestInfo := createRequestInfo(metadata, reqs)
+		resp := &v1alpha1.BidiStreamResponse{
+			Payload: &v1alpha1.ConformancePayload{
+				RequestInfo: requestInfo,
+				Data:        responseDefinition.ResponseData[respNum],
+			},
+		}
+		time.Sleep((time.Duration(responseDefinition.ResponseDelayMs) * time.Millisecond))
+
+		if err := stream.Send(resp); err != nil {
+			return status.Errorf(codes.Internal, "error sending on stream: %s", err.Error())
+		}
+	}
+
+	if responseDefinition.Error != nil {
+		return grpcutil.ConvertProtoToGrpcError(responseDefinition.Error)
+	}
+	return nil
 }
 
 // Parses the given unary response definition and returns either
