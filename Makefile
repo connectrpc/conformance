983fff89bc74ce7a5b35928c249d548970baf4e1
Steve Ayers <sayers@buf.build>
2024-01-17 18:54:42 -0500

    Cleanup

--- Makefile
+++ Makefile
@@ -101,14 +101,14 @@ runservertests: $(BIN)/connectconformance $(BIN)/referenceserver $(BIN)/grpcserv
 	$(BIN)/connectconformance -v --conf ./testdata/grpc-web-server-impl-config.yaml --mode server -- $(BIN)/grpcserver
 
 .PHONY: runclienttests
-runclienttests: $(BIN)/connectconformance $(BIN)/referenceclient $(BIN)/grpcclient
+runclienttests: $(BIN)/connectconformance $(BIN)/referenceclient $(BIN)/grpcclient buildgrpcweb
 	$(BIN)/connectconformance -v --conf ./testdata/reference-impls-config.yaml --mode client -- $(BIN)/referenceclient
 	$(BIN)/connectconformance -v --conf ./testdata/grpc-impls-config.yaml --mode client -- $(BIN)/grpcclient
-	$(MAKE) rungrpcweb
+	$(BIN)/connectconformance -v --conf ./testdata/grpc-web-client-impl-config.yaml --known-failing ./grpcwebclient/known_failing.txt --mode client -- ./grpcwebclient/bin/grpcwebclient
 
-.PHONY: rungrpcweb
-rungrpcweb: generate $(BIN)/connectconformance
-	cd grpcwebclient && npm run conformance
+.PHONY: buildgrpcweb
+buildgrpcweb: generate
+	cd grpcwebclient && npm run build
 
 $(BIN)/connectconformance: Makefile generate
 	$(GO) build $(DEV_BUILD_VERSION_FLAG) -o $(@) ./cmd/connectconformance/
--- grpcwebclient/runner.ts
+++ grpcwebclient/runner.ts
@@ -1,26 +1,34 @@
-// TODO - End result of all this is that we should end up with this file and browserscript.ts as the only files
-// This runs the runner, the other one runs in Puppeteer
+// Copyright 2023-2024 The Connect Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Import puppeteer
 import type { Readable } from "node:stream";
 import puppeteer from "puppeteer";
 import * as esbuild from "esbuild";
 import {
   ClientCompatRequest,
   ClientCompatResponse,
-  ClientResponseResult,
-  ClientErrorResult,
 } from "./gen/proto/connectrpc/conformance/v1/client_compat_pb.js";
 
 export function run() {
-  void main().catch((reason) => {
+  void runTestSuite().catch((reason) => {
     console.error("ITS FAILING");
     // TODO - Write client error result back and exit
     throw reason;
   });
 }
 
-async function main() {
+async function runTestSuite() {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   page.on("pageerror", (err) => {
