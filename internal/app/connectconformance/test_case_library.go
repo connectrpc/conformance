55204cb2bdeb964c99cb6f3d52f3c3bebff464cc
Steve Ayers <sayers@buf.build>
2024-02-14 14:31:05 -0500

    Docs

--- internal/app/connectconformance/test_case_library.go
+++ internal/app/connectconformance/test_case_library.go
@@ -174,18 +174,18 @@ func (lib *testCaseLibrary) expandSuite(suite *conformancev1.TestSuite, configCa
 
 func (lib *testCaseLibrary) expandCases(cfgCase configCase, namePrefix []string, testCases []*conformancev1.TestCase) error {
 	for _, testCase := range testCases {
-		// if testCase.Request.TestName == "" {
-		// 	return errors.New("suite contains a test with no name")
-		// }
-		// if testCase.Request.Service == "" {
-		// 	return fmt.Errorf("test %s has no service specified", testCase.Request.TestName)
-		// }
-		// if testCase.Request.Method == "" {
-		// 	return fmt.Errorf("test %s has no method specified", testCase.Request.TestName)
-		// }
-		// if testCase.Request.StreamType == conformancev1.StreamType_STREAM_TYPE_UNSPECIFIED {
-		// 	return fmt.Errorf("test %s has no stream type specified", testCase.Request.TestName)
-		// }
+		if testCase.Request.TestName == "" {
+			return errors.New("suite contains a test with no name")
+		}
+		if testCase.Request.Service == "" {
+			return fmt.Errorf("test %s has no service specified", testCase.Request.TestName)
+		}
+		if testCase.Request.Method == "" {
+			return fmt.Errorf("test %s has no method specified", testCase.Request.TestName)
+		}
+		if testCase.Request.StreamType == conformancev1.StreamType_STREAM_TYPE_UNSPECIFIED {
+			return fmt.Errorf("test %s has no stream type specified", testCase.Request.TestName)
+		}
 		if testCase.Request.StreamType != cfgCase.StreamType {
 			continue
 		}
